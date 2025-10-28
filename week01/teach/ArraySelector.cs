49ff71e503652dd2b5152249a2497f2bfbe5b810
EM-OBI <gbassey1@byupathway.edu>
2025-10-28 17:55:57 +0100

    Teach_w01

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,24 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var result = new int[select.Length];
+        int used1 = 0;
+        int used2 = 0;
+
+        for (int i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+                result[i] = list1[used1];
+                used1++;
+            }
+            else
+            {
+                result[i] = list2[used2];
+                used2++;
+            }
+        }
+
+        return result;
     }
 }
