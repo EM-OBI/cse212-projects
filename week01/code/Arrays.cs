ac9e8104663bd1915d1e09562e0ddf425fa247a9
EM-OBI <gbassey1@byupathway.edu>
2025-10-31 08:53:53 +0100

    Individual activity - code commit

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,20 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Solution
+        //Create a new array of length "length"
+        var multiples = new double[length];
+        
+        //The loop should run for "length number of times"
+        for (int i = 1; i <= length; i++)
+        {
+        //Each iteration of the loop will add to a variable that starts from one (i), increases by one and multiplies the starting number by whatever the variable is at that point. 
+        //The resulting number is added to the new array at one less than index (i). 
+            double numberMultiplies = number * i;
+            multiples[i - 1] = numberMultiplies;
+        }
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,9 +38,17 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
+
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //First, we get all the "amount" items ((data.Count - amount)) from the end of the list as save it in a new list using GetRange
+        var newList = data.GetRange(data.Count - amount, amount);
+        //Next we remove all matching items from the end of the list using RemoveRange starting from the index where the amount of items can be removed and ending at the end (data.Count - amount)
+        data.RemoveRange(data.Count - amount, amount);
+        //Finally, we append the new list to the old list using InsertRange starting from index 0, effectively rotating the list right
+        data.InsertRange(0, newList);
     }
 }
