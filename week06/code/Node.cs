d3c31979d545a8d46436dc9a05d0b18fa3902f33
EM-OBI <gbassey1@byupathway.edu>
2025-12-03 16:22:49 +0100

    Week 06 Individiual activity commit

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,8 +1,13 @@
+using System.Runtime.InteropServices.Marshalling;
+using Microsoft.VisualBasic;
+
 public class Node
 {
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
+
+    private HashSet<int> unique = new HashSet<int>();
 
     public Node(int data)
     {
@@ -12,34 +17,71 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
-
-        if (value < Data)
+        if (!unique.Contains(value))
         {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
+            unique.Add(value);
+            if (value < Data)
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
             else
-                Left.Insert(value);
-        }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        if (value == Data)
+        {
+            return true;
+        }
+
+        if (value < Data)
+        {
+            if (Left is not null)
+            {
+                return Left.Contains(value);
+            }
+        }
+        else
+        {
+            if (Right is not null)
+            {
+                return Right.Contains(value);
+            } 
+        }
         return false;
     }
+
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left is not null)
+        {
+            leftHeight = Left.GetHeight();
+        }
+        
+        if (Right is not null)
+        {
+            rightHeight = Right.GetHeight();
+        }
+         // Replace this line with the correct return statement(s)
+        return 1 + Math.Max(leftHeight, rightHeight);
+        
     }
 }
